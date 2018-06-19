43b26bcd481773d9351498f233a998a3e8e96e79
Josh Cox <josh@webhosting.coop>
2018-06-19 11:40:56 -0500

    Adding random back in

--- __init__.py
+++ __init__.py
@@ -724,6 +724,38 @@ class NoAgendaSkill(MycroftSkill):
                 self.process.terminate()
                 self.process.wait()
 
+    @intent_handler(IntentBuilder("random").
+        optionally("Play").
+        optionally("random").
+        require("NoAgenda").
+        build())
+    def handle_random_intent(self, message):
+        try:
+            self.stop()
+
+            random_episode = random.randint(0,14)
+            feeddata = feedparser.parse(self.url_rss)
+            data = feeddata.entries[random_episode]
+            # Stop anything already playing
+
+            url = data.enclosures[0]['url']
+            LOG.info('random')
+            LOG.info(random_episode)
+            LOG.info(url)
+
+            # After the intro, start the no agenda stream
+            # if audio service module is available use it
+            wait_while_speaking()
+            if self.audioservice:
+                LOG.info('AudioService')
+                self.audioservice.play(url, message.data['utterance'])
+            else:  # othervice use normal mp3 playback
+                LOG.info('playmp3')
+                self.process = play_mp3(url)
+
+        except Exception as e:
+            LOG.error("Error: {0}".format(e))
+
     @intent_handler(IntentBuilder("latest").
         optionally("Play").
         optionally("latest").
